8f5503b96a3ca581f752f920d88126dc5b5d8c4c
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-10-05 16:47:07 +0900

    06
    실습6

--- main.c
+++ main.c
@@ -3,18 +3,27 @@
 
 int main(int argc, char *argv[])
 {
-   int sum =0;
-   int num;
-   int i;
-   
-   printf("input a number:");
-   scanf("%i", &num);
-   
-   for (i=0; i<=num; i++)
-        sum = sum+i;
+    int answer =32;
+    int num;
+    int trial =0;
     
+    do
+    {
+     printf("enter:\n");
+     scanf("%d",&num);
+     
+      if(num<answer)
+       printf("up\n");
+      else if(num>answer)
+       printf("down\n");
+     
+     trial= trial+1;
+     }
+   while(num!=answer);
+   printf("the end\n");
+   printf("trial:%d",trial);
 
-   printf("result is %i\n", sum);
+
   system("PAUSE");	
   return 0;
 }
